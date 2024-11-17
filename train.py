2de36591c197c4fd0d95bf586a7c797c9a2f9c0b
sigurdurdet <sigurdurdet@hi.is>
2024-11-17 20:41:18 +0000

    added function that gathers parameter -p <path to  dirrectory containing .json file>

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import sys
 
 from model.utils import Params
 from model.utils import set_logger
@@ -10,9 +11,39 @@ from model.model_fn import model_fn
 from model.visualize import metrics_to_plot
 
 
+def check_path(path, file_usage='r'):
+    """Check if a path exists and is accessible."""
+    if not os.path.exists(path):
+        print("Error! The path '{}' does not exist.".format(path))
+        sys.exit(1)
+    return True
+
+def get_parser():
+    """gathering parameters"""
+    parser = argparse.ArgumentParser(description="Path to model_dir")
+    
+    # Define the argument
+    parser.add_argument(
+        '-p', '--path',  # Short and long version of the argument
+        help="Path to parameters from the experiments params.json file in model_dir",
+        required=True    # Make this argument mandatory
+    )
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Check the validity of the provided path
+    check_path(args.path)
+    
+    return args
+
+
 if __name__ == '__main__':
     # Load the parameters from the experiment params.json file in model_dir
-    model_dir = './expirements/mlp/'
+    args = get_parser()
+    #model_dir = './expirements/mlp/'
+    
+    model_dir = args.path
     json_path = os.path.join(model_dir, 'params.json')
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
     params = Params(json_path)
